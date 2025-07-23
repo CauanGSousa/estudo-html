f9d67944463ac245801299eb990e541cbce8af97
CauanGSousa <cauan.gutembergdesousa@gmail.com>
2025-07-23 19:49:58 -0300

    developmentcauan - Calculadora mais estudos js

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,17 @@
-const btn = document.querySelector('#pesquisar-button');
-console.log(btn);
-btn.addEventListener('click', mudarcorbody);
+const gaveta = document.querySelector('#pesquisar-button');
+console.log(gaveta);
+gaveta.addEventListener('click', mudarcorbody);
 
 function mudarcorinput(input) {
+    const valor = input.value > 5;
     if (input.value === 'blue') {
         input.style.backgroundColor = 'blue';
     } else if (input.value === 'red') {
         input.style.backgroundColor = 'red';
     } else if (input.value === 'green') {
         input.style.backgroundColor = 'green';
-    } else {
-        input.style.backgroundColor = 'white';
+    } else if (valor === true) {
+        input.style.backgroundColor = 'yellow';
     }
     const mensagem= 'Não encontrado.';
     printarmensagem(mensagem, input);
@@ -31,3 +32,9 @@ function printarmensagem(msg, input) {
         input.value=msg;
     }, 5000); // Simulate a delay for the message
 }
+
+// Aprendi variaveis (const body) e (let input), funções, eventos e manipulação de DOM com JavaScript.
+// Aprendi estruturas condicionais (if, else if, else) e como alterar o estilo de elementos HTML.
+// Aprendi a usar o método setTimeout para simular um atraso na exibição de mensagens
+//Aprendi tipos de dados primitivos (string, number, boolean) e como usá-los em JavaScript.
+//Aprendi seletores de elementos do DOM (querySelector) e como manipulá-los com JavaScript.
